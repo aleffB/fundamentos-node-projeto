3c73243b8807397ca305ea1bb203da85096a90cb
aleffB <aleff.bruno96@hotmail.com>
2021-02-17 12:28:38 -0300

    :sparkles: challenge finalized

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import BalanceTransactionService from '../services/BalanceTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
+
+const transactionRepository = new TransactionsRepository();
 
 const transactionRouter = Router();
 
@@ -9,15 +14,27 @@ const transactionRouter = Router();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+   const transactions = transactionRepository.all();
+    const balance = transactionRepository.getBalance();
+
+    return response.json({transactions, balance});
+
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
+
+
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionService(transactionRepository);
+
+    const transaction = createTransaction.execute({title, value, type});
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
